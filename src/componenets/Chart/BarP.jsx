1687ccc3b687f758d7c879d9949286d1b9ac8239
mariemzghdi <zghidimariem@gmail.com>
2022-02-15 22:53:03 +0100

    notf

--- src/componenets/Chart/BarP.jsx
+++ src/componenets/Chart/BarP.jsx
@@ -1,78 +1,75 @@
-import React ,{useEffect,useState}from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
-import './Chart.css';
-import DataThresholdingIcon from '@mui/icons-material/DataThresholding';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import LinearProgress, {
+  linearProgressClasses,
+} from "@mui/material/LinearProgress";
+import "./Chart.css";
+import DataThresholdingIcon from "@mui/icons-material/DataThresholding";
+import axios from "axios";
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
-    height: 25,
-    borderRadius: 5,
-    [`&.${linearProgressClasses.colorPrimary}`]: {
-      backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
-    },
-    [`& .${linearProgressClasses.bar}`]: {
-      borderRadius: 10,
-      backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
-    },
-  }));
+  height: 25,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor:
+      theme.palette.grey[theme.palette.mode === "light" ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 10,
+    backgroundColor: theme.palette.mode === "light" ? "#1a90ff" : "#308fe8",
+  },
+}));
 
 export default function BarP() {
-  const [rows,setRows]= useState([]);
-  
+  const [rows, setRows] = useState([]);
 
-
-  
   async function getDonnees() {
-    const reponse = await axios.get(`http://localhost:8080/api/controle_packet/qte`)
-    return(setRows(reponse.data) )
-  
-    
-    
-}
-useEffect(()=>{
-  getDonnees();
-},[])
-
-  const Qte = rows.map(obj =>{
-  return parseFloat(obj.QteT); 
-}) 
-console.log(rows);
-console.log(Qte);
-
-
-function numAverage(Qte) {
-  var b = Qte.length,
-      c = 0, i;
-  for (i = 0; i < b; i++){
-    c += Number(Qte[i]);
+    const reponse = await axios.get(
+      `http://localhost:8080/api/controle_packet/qte`
+    );
+    return setRows(reponse.data);
   }
-  return c;
+  useEffect(() => {
+    getDonnees();
+  }, []);
 
-}
-console.log(numAverage(Qte));
+  const Qte = rows.map((obj) => {
+    return parseFloat(obj.QteT);
+  });
+  console.log(rows);
+  console.log(Qte);
 
+  function numAverage(Qte) {
+    var b = Qte.length,
+      c = 0,
+      i;
+    for (i = 0; i < b; i++) {
+      c += Number(Qte[i]);
+    }
+    return c;
+  }
+  console.log(numAverage(Qte));
 
-  return <div> 
-        <div className="gaugechart">
-        <div className =" subnavbar">
+  return (
+    <div>
+      <div className="gaugechart">
+        <div className="subnavbar">
           <span className="titlePichart">Qte Produites</span>
           <DataThresholdingIcon className="helpIcon" />
-         
-           <hr className="Separtormini" /> 
+
+          <hr className="Separtormini" />
         </div>
-     
-  <Box sx={{ flexGrow: 1 }}>
-  <br />
-  <BorderLinearProgress variant="determinate" value={numAverage(Qte)-40}  className='LineProg'/>
-   <span className='Comments'
-  
-    >Qte  :  {numAverage(Qte)} </span>  
-  </Box>
-  </div>
-        
+
+        <Box sx={{ flexGrow: 1 }}>
+          <br />
+          <BorderLinearProgress
+            variant="determinate"
+            value={numAverage(Qte) - 40}
+            className="LineProg"
+          />
+          <span className="Comments">Qte : {numAverage(Qte)} </span>
+        </Box>
       </div>
-
-
-;
+    </div>
+  );
 }
--- src/componenets/Chart/Colone_Chart.jsx
+++ src/componenets/Chart/Colone_Chart.jsx
@@ -1,71 +1,71 @@
-import React,{useState,useEffect} from 'react';
-import Chart from 'react-apexcharts';
-import AddchartIcon from '@mui/icons-material/Addchart';
-import axios from 'axios';
-import './Chart.css';
-
-
-
+import React, { useState, useEffect } from "react";
+import Chart from "react-apexcharts";
+import AddchartIcon from "@mui/icons-material/Addchart";
+import axios from "axios";
+import "./Chart.css";
 
 export default function Colone_Chart() {
-  const [rows,setRows]= useState([]);
-  
+  const [rows, setRows] = useState([]);
 
-
-  
   async function getDonnees() {
-    const reponse = await axios.get(`http://localhost:8080/api/pack_operation/Rend`)
-    return(setRows(reponse.data) )
-  
-    
-    
-}
-useEffect(()=>{
-  getDonnees();
-},[])
+    const reponse = await axios.get(
+      `http://localhost:8080/api/pack_operation/Rend`
+    );
+    return setRows(reponse.data);
+  }
+  useEffect(() => {
+    getDonnees();
+  }, []);
 
-  const Rendement = rows.map(obj =>{
-  return parseFloat(obj.Rend); 
-}) 
-const Noms = rows.map(obj =>{
-  return (obj.Firstname); 
-}) 
- console.log(Rendement)
-console.log(Noms)
+  const Rendement = rows.map((obj) => {
+    return parseFloat(obj.Rend);
+  });
+  const Noms = rows.map((obj) => {
+    return obj.Firstname;
+  });
+  console.log(Rendement);
+  console.log(Noms);
 
-    const   options= {
-        chart: {
-          id: 'apexchart-example',
- 
+  const options = {
+    chart: {
+      id: "apexchart-example",
+    },
+
+    colors: ["#1a90ff"],
+
+    xaxis: {
+      categories: Noms,
+
+      labels: {
+        style: {
+          fontSize: "12px",
         },
-       
-        colors: ['#1a90ff', ],
-        
-        
-        xaxis: {
-          categories: Noms,
-        
-          labels: {
-            style: {
-             
-              fontSize: '12px'
-            }
-          }}
-      };
-      
-      const series = [{
-        name: 'Rendement',
-        data: Rendement
-      }];
+      },
+    },
+  };
 
-  return(           
-  <div className="gaugechart">
-  <div className="subnavbar">
-    <span className="titlePichart">Chart</span>
-    <AddchartIcon className="helpIcon" />
-   
-    <hr className="Separtormini" />
-  </div>
-  <Chart options={options} series={series} type="bar" width={650} height={350}  />
-</div>)
+  const series = [
+    {
+      name: "Rendement",
+      data: Rendement,
+    },
+  ];
+
+  return (
+    <div className="gaugechart">
+      <div className="subnavbar">
+        <span className="titlePichart">Chart</span>
+        <AddchartIcon className="helpIcon" />
+
+        <hr className="Separtormini" />
+      </div>
+      <Chart
+        options={options}
+        series={series}
+        type="bar"
+        width={650}
+        height={350}
+      />
+    </div>
+  );
 }
